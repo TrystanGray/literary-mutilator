93164adbf574c46026a2880aaf94a642bae0ef51
Trystan Gray <trystangray7@gmail.com>
2019-05-15 16:37:55 -0600

    fixed a tad

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const para = document.querySelector('p');
 para.addEventListener('click', updateName);
 
 function updateName() {
-	let name = prompt('Enter a new message');
-	para.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was:' + name;
+	let Saying = prompt('Enter a new message');
+	para.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
 }
