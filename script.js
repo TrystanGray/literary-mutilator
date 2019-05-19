7071f411ba98880ec96b4ad63045f251519da5bd
Trystan Gray <trystangray7@gmail.com>
2019-05-19 13:32:07 -0600

    added comments

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+// a little easter egg ;) wish i could do it on all . maybe w class?
 const para = document.getElementById('1');
 para.addEventListener('click', updateName);
 function updateName() {
 	let Saying = prompt('Enter a new message');
 	para.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
 }
-
+//changing color of words
 function changeColor(newColor) {
 	var elem = document.getElementById('1');
 	elem.style.color = newColor;
 }
-
+//randomizing words, took me so damn long to find anything
 function mixWords() {
 	let stuff = document.getElementById("2").textContent;
 	let words = stuff.split(" ");
@@ -23,7 +24,7 @@ function mixWords() {
 		document.getElementById("2").textContent = words.join(" ");
 	});
 }
-
+// highlighting paragraph
 function highlight() {
 	document.getElementById("3").style.backgroundColor = "#fdff32";
 }
