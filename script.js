f7047ab3a0cf2fe76496ca9d85b7b40e24c24b34
Trystan Gray <trystangray7@gmail.com>
2019-05-19 13:44:26 -0600

    cheat complete

--- script.js
+++ script.js
@@ -2,14 +2,30 @@
 const para = document.getElementById('1');
 para.addEventListener('click', updateName);
 function updateName() {
-	let Saying = prompt('Enter a new message');
+	let Saying = prompt('What do you have to say for yourself mortal?');
 	para.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
 }
+const easter = document.getElementById('2');
+easter.addEventListener('click', updateName);
+function updateName() {
+	let Saying = prompt('What do you have to say for yourself mortal?');
+	easter.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
+}
+const egg = document.getElementById('3');
+egg.addEventListener('click', updateName);
+function updateName() {
+	let Saying = prompt('What do you have to say for yourself mortal?');
+	egg.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
+}
+
+
 //changing color of words
 function changeColor(newColor) {
 	var elem = document.getElementById('1');
 	elem.style.color = newColor;
 }
+
+
 //randomizing words, took me so damn long to find anything
 function mixWords() {
 	let stuff = document.getElementById("2").textContent;
@@ -24,6 +40,8 @@ function mixWords() {
 		document.getElementById("2").textContent = words.join(" ");
 	});
 }
+
+
 // highlighting paragraph
 function highlight() {
 	document.getElementById("3").style.backgroundColor = "#fdff32";
