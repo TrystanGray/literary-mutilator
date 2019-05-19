982c3d2e85fdae5c2d49a456abc9e11b476e2643
Trystan Gray <trystangray7@gmail.com>
2019-05-19 12:28:25 -0600

    added buttons for red and blue

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const para = document.querySelector('p');
-
+const para = document.getElementById('1');
 para.addEventListener('click', updateName);
-
 function updateName() {
 	let Saying = prompt('Enter a new message');
 	para.textContent = 'Oh shit Morty, l-l-look this dumb ass broke our paragraph Morty, and the only thing this glip glop typed was: ' + Saying;
+}
+
+function changeColor(newColor) {
+	var elem = document.getElementById('1');
+	elem.style.color = newColor;
 }
